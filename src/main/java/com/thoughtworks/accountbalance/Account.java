791c3d2fe7b7e768a09ae48432d854291b89ba1a
Cannon Biggs <cebiggs@unomaha.edu>
2015-06-05 20:05:38 -0500

    Implement deposit() method and test

---
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.accountbalance;
+
+/**
+ * Created by cannon on 6/5/15.
+ */
+public class Account {
+
+    public double balance;
+
+    public void deposit( double deposit ) {
+        balance += deposit;
+    }
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -1,13 +1,18 @@
 package com.thoughtworks.accountbalance;
 
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        Account account = new Account();
+        account.balance = 100;
+        account.deposit(50);
+        assertThat(account.balance, is(150.00));
+    }
+
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 //
